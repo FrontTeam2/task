9443e86ecfebdb76763d1306d44af310ed367866
beenbin <showme0241@gmail.com>
2023-01-25 00:54:48 +0900

    level2 난제
    value값이 사라졌을 때, 결국 일치하지 않는 결과를 조회함

--- level2/level2.js
+++ level2/level2.js
@@ -9,15 +9,18 @@ console.log($reserNum);
 
 function comparisonData() {
     // 필터를 돌릴 경우, 함수에 맞는 결과가 나와야되는디...
-
     RESERVATION_LIST.filter((item) => {
-        if ($userName.value === item.name && $userTel.value === item.phone) {
-            return ($reserNum.innerText = `${item.number}`);
+        let newArray = [];
+        if ($userName.value === item.name) {
+            if ($userTel.value === item.phone) {
+                newArray.push(item.number);
+            } else {
+                $reserNum.innerText = "연락처를 확인하세요.";
+            }
+        } else {
+            $reserNum.innerText = "예약정보가 일치하지 않습니다.";
         }
-
-        if ($userName.value !== item.name && $userTel.value !== item.phone) {
-            return ($reserNum.innerText = "예약정보가 일치하지 않습니다.");
-        }
+        return ($reserNum.innerText = newArray);
     });
     // for (let i = 0; i < RESERVATION_LIST.length; i++) {
     //     if (
