e5f1d60628f0dc9d2daa97196866ebf48f7397ef
twosky0202 <leehaneul0202@naver.com>
2023-01-25 00:56:17 +0900

    level1 퀴즈 완료, level2 퀴즈 고민해보기

--- level1/level1.js
+++ level1/level1.js
@@ -5,7 +5,15 @@
 ex) 길이기 80인 배열은 길이가 5로 분해한다면 16개의 배열을 요소로 갖는 배열을 반환한다
 */
 
-function division(arr, num) {}
+var newArr=[];
+function division(array, n) {
+    for(let i=0;i<array.length;i=i+n){
+        newArr.push(array.slice(i, i+n));
+    }
+    return newArr;
+}
+var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+console.log(division(arr, 3));
 
 // arr = [1,2,3,4,5]
 // divition(arr, 2); === [ [1,2], [3,4], [5] ]
--- level2/level2.js
+++ level2/level2.js
@@ -3,6 +3,33 @@ console.log(RESERVATION_LIST);
 
 /* 
 예약 고객확인하기
-
-
 */
+var name = document.getElementsByName('user-name')[0];
+var phone = document.getElementsByName('user-phone')[0];
+var number = document.getElementById('reservation-number');
+// function checkNumber(){
+//     for(let i = 0; i < RESERVATION_LIST.length; i++){
+//         if(name.value == RESERVATION_LIST[i].name && phone.value == RESERVATION_LIST[i].phone){
+//             number.innerText = RESERVATION_LIST[i].number;
+//         }else{
+//             number.innerText = "예약번호가 없습니다.";
+//         }
+//     }
+// }
+
+// document.getElementById('btn').addEventListener('click', checkNumber);
+
+document.getElementById('form').onsubmit = function(){
+    for(let i = 0; i < RESERVATION_LIST.length; i++){
+        if(name.value === RESERVATION_LIST[i].name && phone.value === RESERVATION_LIST[i].phone){
+            number.innerText = RESERVATION_LIST[i].number;
+        }else{
+            number.innerText = "예약번호가 없습니다.";
+        }
+    }
+    console.log(name.value);
+    console.log(phone.value);
+    console.log(RESERVATION_LIST[0].name);
+
+    return false;
+}
