90187b03b0ef2119ebf8d906d97347870d024e96
beenbin <showme0241@gmail.com>
2023-01-26 01:09:14 +0900

    level2 해결완료
    동영님, 하늘님 리뷰 BEST

--- level2/level2.js
+++ level2/level2.js
@@ -7,30 +7,36 @@ const $userFindBtn = document.querySelector("#user_find");
 const $reserNum = document.querySelector("#reservation-number");
 console.log($reserNum);
 
-function comparisonData() {
-    // 필터를 돌릴 경우, 함수에 맞는 결과가 나와야되는디...
+function comparisonData(e) {
+    e.preventDefault();
+
+    let newArray = [];
     RESERVATION_LIST.filter((item) => {
-        let newArray = [];
         if ($userName.value === item.name) {
             if ($userTel.value === item.phone) {
-                newArray.push(item.number);
-            } else {
-                $reserNum.innerText = "연락처를 확인하세요.";
+                newArray.push(item);
             }
-        } else {
-            $reserNum.innerText = "예약정보가 일치하지 않습니다.";
         }
-        return ($reserNum.innerText = newArray);
     });
+    console.log(newArray);
+    if (!(newArray === "")) {
+        // ===를 사용하였을 때, 타입(어떤 타입이 다른지 확인하면 해결 완료)이
+        // 다르기 때문에 false를 반환하여 else문이 호출됨 -> number는 빈 배열에 없기 때문에
+        // undefined로 error가 생김
+        $reserNum.innerText = "예약정보와 일치하지 않습니다.";
+    } else {
+        $reserNum.innerText = newArray[0].number;
+    }
+    console.log(newArray);
     // for (let i = 0; i < RESERVATION_LIST.length; i++) {
     //     if (
     //         $userName.value === RESERVATION_LIST[i].name &&
     //         $userTel.value === RESERVATION_LIST[i].phone
     //     ) {
     //         $reserNum.innerText = `${RESERVATION_LIST[i].number}`;
-    //         alert($reserNum.innerText);
+    //         break;
     //     } else {
-    //         alert("예약정보가 일치하지 않습니다.");
+    //         $reserNum.innerText = "예약정보가 일치하지 않습니다.";
     //     }
     // }
 }
@@ -39,6 +45,4 @@ $userFindBtn.addEventListener("click", comparisonData);
 
 /* 
 예약 고객확인하기
-
-
 */
