4a06aa16d09a205b5ffbba83a5584dd806698c21
twosky0202 <leehaneul0202@naver.com>
2023-01-26 01:07:12 +0900

    level2 완료, level3 이어서하기

--- level2/level2.js
+++ level2/level2.js
@@ -4,32 +4,45 @@ console.log(RESERVATION_LIST);
 /* 
 예약 고객확인하기
 */
-var name = document.getElementsByName('user-name')[0];
-var phone = document.getElementsByName('user-phone')[0];
-var number = document.getElementById('reservation-number');
-// function checkNumber(){
-//     for(let i = 0; i < RESERVATION_LIST.length; i++){
-//         if(name.value == RESERVATION_LIST[i].name && phone.value == RESERVATION_LIST[i].phone){
-//             number.innerText = RESERVATION_LIST[i].number;
-//         }else{
-//             number.innerText = "예약번호가 없습니다.";
-//         }
-//     }
-// }
+const name = document.getElementsByName('user-name')[0];
+const phone = document.getElementsByName('user-phone')[0];
+const number = document.getElementById('reservation-number');
+const btn = document.getElementById('btn');
 
-// document.getElementById('btn').addEventListener('click', checkNumber);
-
-document.getElementById('form').onsubmit = function(){
+function checkNumber(e){
+    e.preventDefault();
     for(let i = 0; i < RESERVATION_LIST.length; i++){
-        if(name.value === RESERVATION_LIST[i].name && phone.value === RESERVATION_LIST[i].phone){
+        if(name.value == RESERVATION_LIST[i].name && phone.value == RESERVATION_LIST[i].phone){
             number.innerText = RESERVATION_LIST[i].number;
+            break;
         }else{
             number.innerText = "예약번호가 없습니다.";
         }
     }
-    console.log(name.value);
-    console.log(phone.value);
-    console.log(RESERVATION_LIST[0].name);
-
-    return false;
 }
+btn.addEventListener('click', checkNumber);
+/* <form>안에 <button>태그가 있는 경우에는 <button>클릭시 기본적으로 페이지가 reload된다. <button>의 기본 타입이 submit.
+이를 막기 위해서는 아래와 같이 자바스크립트 preventDefault 로 기본동작을 막을 수 있다.
+
+function(e)) {
+    e.preventDefault();
+}
+혹은 
+
+button 태그에 type속성을 button으로 주면 페이지가 새로고침되지 않는다.
+<button type="button"></button> */
+
+
+// const form = document.getElementById('form');
+
+// form.onsubmit = function(){
+//     for(let i = 0; i < RESERVATION_LIST.length; i++){
+//         if(name.value === RESERVATION_LIST[i].name && phone.value === RESERVATION_LIST[i].phone){
+//             number.innerText = RESERVATION_LIST[i].number;
+//             break;
+//         }else{
+//             number.innerText = "예약번호가 없습니다.";
+//         }
+//     }
+//     return false;
+// }
--- level3/level3.js
+++ level3/level3.js
@@ -2,3 +2,38 @@
 레시피 재료 추가하기
 
 */
+const ingredient = document.getElementsByName('ingredient')[0];
+const weight = document.getElementsByName('weight')[0];
+const add_button = document.getElementById('add_button');
+const submit_button = document.getElementById('submit_button');
+const table = document.getElementById('table');
+const tr = document.createElement('tr');
+const td = document.createElement('td');
+
+const ul = document.getElementById('ingredient-list');
+const li = document.createElement('li');
+
+function add(e){
+    e.preventDefault();
+    const table = document.getElementById('table');
+    const tr = document.createElement('tr');
+    const td1 = document.createElement('td');
+    const td2 = document.createElement('td');
+    const td3 = document.createElement('td');
+    table.appendChild(tr);
+    tr.appendChild(td1);
+    td1.innerText=ingredient.value;
+    tr.appendChild(td2);
+    td2.innerText=weight.value;
+    tr.appendChild(td3);
+    
+    console.log(ingredient.value);
+    console.log(weight.value);
+}
+add_button.addEventListener('click', add);
+
+function submit(e){
+    e.preventDefault();
+}
+
+submit_button.addEventListener('click', submit);
