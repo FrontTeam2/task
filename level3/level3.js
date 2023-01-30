d3774d5c98c1ad78299bd1e345817d7a948f1cd2
twosky0202 <leehaneul0202@naver.com>
2023-01-31 01:01:57 +0900

    level3 수정or삭제 후 제출 구현 아직 못함

--- level3/level3.js
+++ level3/level3.js
@@ -2,81 +2,95 @@
 레시피 재료 추가하기
 
 */
-const ingredient = document.getElementsByName('ingredient')[0];
-const weight = document.getElementsByName('weight')[0];
+const ingredient = document.getElementsByName("ingredient")[0];
+const weight = document.getElementsByName("weight")[0];
 
-const add_button = document.getElementById('add_button');
-const submit_button = document.getElementById('submit_button');
+const add_button = document.getElementById("add_button");
+const submit_button = document.getElementById("submit_button");
 
-const ul = document.getElementById('ingredient-list');
+const ul = document.getElementById("ingredient-list");
 
-let sumbitData = new Array();
-let ingredientData;
-let weightData;
-function add(e){
+// let saveData = new Array();
+let submitData = [];
+function finallistSave() {}
+
+function add(e) {
     e.preventDefault();
+    if (ingredient.value == "") {
+        alert("재료를 입력해주세요");
+    } else if (weight.value == "") {
+        alert("용량을 입력해주세요");
+    } else {
+        const table = document.getElementById("table");
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+        const input1 = document.createElement("input");
+        const input2 = document.createElement("input");
+        const modi_btn = document.createElement("button");
+        const del_btn = document.createElement("button");
 
-    const table = document.getElementById('table');
-    const tr = document.createElement('tr');
-    const td1 = document.createElement('td');
-    const td2 = document.createElement('td');
-    const td3 = document.createElement('td');
-    const input1 = document.createElement('input');
-    const input2 = document.createElement('input');
-    const modi_btn = document.createElement('button');
-    const del_btn = document.createElement('button');
+        table.appendChild(tr);
 
-    table.appendChild(tr);
+        tr.appendChild(td1);
+        input1.value = ingredient.value;
+        input1.readOnly = true;
+        td1.appendChild(input1);
 
-    tr.appendChild(td1);
-    input1.value = ingredient.value;
-    input1.readOnly = true;
-    td1.appendChild(input1);
+        tr.appendChild(td2);
+        input2.value = weight.value;
+        input2.readOnly = true;
+        td2.appendChild(input2);
 
-    tr.appendChild(td2);
-    input2.value = weight.value;
-    input2.readOnly = true;
-    td2.appendChild(input2);
+        tr.appendChild(td3);
+        modi_btn.innerText = "수정";
+        del_btn.innerText = "삭제";
+        td3.appendChild(modi_btn);
+        td3.appendChild(del_btn);
 
-    tr.appendChild(td3);
-    modi_btn.innerText = '수정';
-    del_btn.innerText = '삭제';
-    td3.appendChild(modi_btn);
-    td3.appendChild(del_btn);
+        submitData.push({
+            ingredient: input1.value,
+            weight: input2.value,
+            id: Date.now(),
+        });
+        console.log(submitData);
 
-    modi_btn.addEventListener('click', ()=>{
-        if(modi_btn.innerText == '수정'){
+        ingredient.value = "";
+        weight.value = "";
+    }
+
+    modi_btn.addEventListener("click", (e) => {
+        if (modi_btn.innerText == "수정") {
             input1.readOnly = false;
             input2.readOnly = false;
-            modi_btn.innerText = '완료';
-        } else if(modi_btn.innerText == '완료'){
+            modi_btn.innerText = "완료";
+        } else if (modi_btn.innerText == "완료") {
             input1.readOnly = true;
             input2.readOnly = true;
-            modi_btn.innerText = '수정';
+            modi_btn.innerText = "수정";
         }
-        console.log(input1.value);
     });
-    del_btn.addEventListener('click', ()=>{
+    del_btn.addEventListener("click", () => {
         tr.remove();
+        submitData = submitData.filter(
+            (data) =>
+                data.ingredient != input1.value && data.weight != input2.value
+        );
+        console.log(submitData);
     });
-
-    function submit(e){
-        e.preventDefault();
-        
-        sumbitData.push({
-            ingredientData : input1.value,
-            weightData : input2.value
-        });
-        console.log(sumbitData);
-        for(let i = 0;i<sumbitData.length;i++){
-            const li = document.createElement('li');
-            ul.appendChild(li);
-            li.innerText = sumbitData[i].ingredientData+'/'+sumbitData[i].weightData;
-            console.log(i);
-            console.log(sumbitData[i].ingredientData);
-        }
-    }
-    submit_button.addEventListener('click', submit);
 }
-add_button.addEventListener('click', add);
+add_button.addEventListener("click", add);
 
+function submit(e) {
+    e.preventDefault();
+
+    console.log(submitData);
+
+    for (let i = 0; i < submitData.length; i++) {
+        const li = document.createElement("li");
+        ul.appendChild(li);
+        li.innerText = submitData[i].ingredient + "/" + submitData[i].weight;
+    }
+}
+submit_button.addEventListener("click", submit);
